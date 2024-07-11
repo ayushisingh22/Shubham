c8cd0e0af65a91f210b0b5ba2ad30102f2817881
chauh <chauh@Ayushi>
2024-07-11 13:05:27 +0530

    ok

---
+++ ayushi/src/ayushi/SixthProgram.java
@@ -0,0 +1,17 @@
+package ayushi;
+
+public class SixthProgram {
+	
+	private String name;
+	private int income;
+	private String gender;
+	private int age;
+	private int tax;
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
